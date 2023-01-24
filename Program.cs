40b021e30962bc06d204168a74caffc9488ba816
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-01-25 07:02:13 +1300

    Files: Add all files

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TimetableRightNow // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
